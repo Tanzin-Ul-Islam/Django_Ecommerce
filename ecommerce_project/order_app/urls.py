d35a44012984335e3bb489ec5d9459b0c1064d98
Tanzin-Ul-Islam <tanzin.cse@gmail.com>
2020-12-24 11:59:56 +0600

    commit6

--- ecommerce_project/order_app/urls.py
+++ ecommerce_project/order_app/urls.py
@@ -5,5 +5,8 @@ app_name = "order_app"
 
 urlpatterns = [
     path('add/<pk>/', views.add_to_cart, name='add_to_cart'),
-    path('cart/', views.cart_view, name='cart')
+    path('cart/', views.cart_view, name='cart'),
+    path('remove_item/<pk>/', views.remove_cart_item, name="remove_cart_item"),
+    path('increase_quantity/<pk>/', views.increase_item_quantity, name="increase"),
+    path('decrease_quantity/<pk>/', views.decrease_item_quantity, name="decrease"),
 ]
--- ecommerce_project/order_app/views.py
+++ ecommerce_project/order_app/views.py
@@ -16,11 +16,11 @@ def add_to_cart(request, pk):
             order_item[0].quantity += 1
             order_item[0].save()
             messages.info(request, "This item quantity was updated.")
-            return redirect('shop_app:home')
+            return redirect('order_app:cart')
         else:
             order.orderitems.add(order_item[0])
             messages.info(request,"Item has been added to cart")
-            return redirect('shop_app:home')
+            return redirect('order_app:cart')
     else:
         order = Order(user = request.user)
         order.save()
@@ -35,3 +35,77 @@ def cart_view(request):
     if carts.exists() and order.exists():
         order = order[0]
         return render(request, 'order_app/cart.html', context={'carts':carts, 'order':order})
+    else:
+        return render(request, 'order_app/cart.html', context={'message':"Cart is empty!"})
+@login_required
+def remove_cart_item(request, pk):
+    item = get_object_or_404(Product, pk=pk)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.orderitems.filter(item=item).exists():
+            order_item = Cart.objects.filter(item = item, user= request.user, purchased = False)
+            order_item = order_item[0]
+            order.orderitems.remove(order_item)
+            order_item.delete()
+            if order.orderitems.count() == 0:
+                messages.info(request, "You don't have an active order!")
+                return redirect("shop_app:home")
+            else:
+                messages.warning(request, "Item was removed from your cart!")
+                return redirect("order_app:cart")
+        else:
+            messages.info(request, "This item is not in your cart.")
+            return redirect("shop_app:home")
+    else:
+        messages.info(request, "You don't have any order!")
+        return redirect("shop_app:home")
+@login_required
+def increase_item_quantity(request, pk):
+    item = get_object_or_404(Product, pk=pk)
+    order_qs = Order.objects.filter(user=request.user, ordered = False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.orderitems.filter(item=item).exists():
+            order_item = Cart.objects.filter(item=item, user=request.user, purchased=False)[0]
+            if order_item.quantity >= 1:
+                order_item.quantity += 1
+                order_item.save()
+                return redirect("order_app:cart")
+            else:
+                messages.info(request, "This item is not included to the cart!")
+                return redirect("order_app:cart")
+
+        else:
+            messages.info(request, "This item is not in your cart.")
+            return redirect("shop_app:home")
+
+    else:
+        messages.info(request, "You don't have an active order!")
+        return redirect("shop_app:home")
+
+@login_required
+def decrease_item_quantity(request, pk):
+    item = get_object_or_404(Product, pk=pk)
+    order_qs = Order.objects.filter(user=request.user, ordered = False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.orderitems.filter(item=item).exists():
+            order_item = Cart.objects.filter(item=item, user=request.user, purchased=False)[0]
+            if order_item.quantity > 1:
+                order_item.quantity -= 1
+                order_item.save()
+                return redirect("order_app:cart")
+            else:
+                order.orderitems.remove(order_item)
+                order_item.delete()
+                messages.info(request, "Item has been removed from the cart!")
+                return redirect("order_app:cart")
+
+        else:
+            messages.info(request, "This item is not in your cart.")
+            return redirect("shop_app:home")
+
+    else:
+        messages.info(request, "You don't have an active order!")
+        return redirect("shop_app:home")
